77855226ca1ac0e9552305daef164176afdeed1a
luckkrit <luckkrit@hotmail.com>
2024-03-06 11:47:14 +0700

    add folded note and open note menu function.

--- src/components/draft2.stories.tsx
+++ src/components/draft2.stories.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/util";
 import {
   FloatingFocusManager,
   autoPlacement,
@@ -24,6 +25,7 @@ import {
   GrUnorderedList,
 } from "react-icons/gr";
 import { IoOpenOutline } from "react-icons/io5";
+import { MdCloseFullscreen } from "react-icons/md";
 import { PiDotsThree } from "react-icons/pi";
 const Note = () => {
   return <></>;
@@ -48,10 +50,27 @@ const NoteEditor = ({ editable = false }: NoteEditorProps) => {
   });
   return <EditorContent editor={editor} />;
 };
-
-const NoteMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
+interface NoteMenuProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  open?: boolean;
+  hasOpened?: boolean;
+  onOpen?: () => void;
+  onClose?: () => void;
+  onDelete?: () => void;
+}
+const NoteMenu = ({
+  open = false,
+  hasOpened = false,
+  onOpen,
+  onClose,
+  onDelete,
+}: NoteMenuProps) => {
+  const [isOpen, setIsOpen] = useState(open);
+  const [isOpenNote, setIsOpenNote] = useState(hasOpened);
+  // useEffect(() => {
+  //   if (open === false) {
+  //     setIsOpen(() => open);
+  //   }
+  // }, [open]);
   const { refs, floatingStyles, context } = useFloating({
     open: isOpen,
     onOpenChange: setIsOpen,
@@ -76,16 +95,40 @@ const NoteMenu = () => {
       {isOpen && (
         <FloatingFocusManager context={context} modal={false}>
           <div
-            className="bg-slate-100"
+            className="bg-slate-100 absolute z-50"
             ref={refs.setFloating}
             style={floatingStyles}
             {...getFloatingProps()}
           >
-            <NoteMenuItem onMenuItemClick={() => setIsOpen(false)}>
-              <IoOpenOutline />
-              <div>Open Note</div>
+            <NoteMenuItem
+              onMenuItemClick={() => {
+                if (!isOpenNote) {
+                  onOpen && onOpen();
+                } else {
+                  onClose && onClose();
+                }
+                setIsOpen(false);
+                setIsOpenNote((isOpenN) => !isOpenN);
+              }}
+            >
+              {isOpenNote ? (
+                <>
+                  <MdCloseFullscreen />
+                  <div>Close Note</div>
+                </>
+              ) : (
+                <>
+                  <IoOpenOutline />
+                  <div>Open Note</div>
+                </>
+              )}
             </NoteMenuItem>
-            <NoteMenuItem onMenuItemClick={() => setIsOpen(false)}>
+            <NoteMenuItem
+              onMenuItemClick={() => {
+                setIsOpen(false);
+                onDelete && onDelete();
+              }}
+            >
               <BsTrash />
               <div>Delete Note</div>
             </NoteMenuItem>
@@ -118,15 +161,91 @@ type Story = StoryObj<typeof meta>;
 
 export const Draft1: Story = {
   render: () => {
+    const [count, setCount] = useState(0);
+    const [openMenu, setOpenMenu] = useState(false);
+    const [isFolded, setIsFolded] = useState(false);
     return (
-      <div className="group/note w-fit h-fit drop-shadow-md ">
-        <div className="flex flex-col bg-amber-100 border-amber-200 w-[220px] h-[200px] group-hover/note:bg-amber-950/10 group-hover/note:shadow-md">
+      <div
+        className="group/note w-fit h-fit drop-shadow-md"
+        onMouseEnter={() => {
+          setCount((o) => o + 1);
+          setOpenMenu(() => false);
+        }}
+        onMouseLeave={() => {
+          setCount((o) => o + 1);
+          setOpenMenu(() => false);
+        }}
+      >
+        {isFolded && (
+          <div className="absolute top-0 right-0 group/note w-fit h-fit drop-shadow-md ">
+            <div className="flex flex-col bg-amber-100 border-amber-200 w-[220px] h-[200px] ">
+              <div className="flex justify-between bg-amber-200 h-0 group-focus-within/note:h-8 transition-all has-[:hover]:h-8">
+                <div>
+                  <button className="invisible group-focus-within/note:visible hover:visible hover:bg-zinc-200/60 p-2">
+                    <GrAdd className="invisible group-focus-within/note:visible hover:visible" />
+                  </button>
+                </div>
+                <div>
+                  <button className="invisible group-focus-within/note:visible hover:visible hover:bg-zinc-200/60 p-2">
+                    <PiDotsThree className="invisible group-focus-within/note:visible hover:visible" />
+                  </button>
+                  <button className="invisible group-focus-within/note:visible hover:visible hover:bg-zinc-200/60 p-2">
+                    <GrClose className="invisible group-focus-within/note:visible hover:visible" />
+                  </button>
+                </div>
+              </div>
+              <div className="fixed top-8 bottom-8 left-1 right-1">
+                <NoteEditor editable />
+              </div>
+              <div className="fixed bottom-0 flex justify-between invisible group-focus-within/note:visible transition-all has-[:hover]:visible border-t border-t-stone-200 w-full p-1">
+                <div>
+                  <button className="invisible group-focus-within/note:visible hover:visible hover:bg-zinc-200/60 p-2">
+                    <GrBold className="invisible group-focus-within/note:visible hover:visible" />
+                  </button>
+                  <button className="invisible group-focus-within/note:visible hover:visible hover:bg-zinc-200/60 p-2">
+                    <GrItalic className="invisible group-focus-within/note:visible hover:visible" />
+                  </button>
+                  <button className="invisible group-focus-within/note:visible hover:visible hover:bg-zinc-200/60 p-2">
+                    <GrUnderline className="invisible group-focus-within/note:visible hover:visible" />
+                  </button>
+                  <button className="invisible group-focus-within/note:visible hover:visible hover:bg-zinc-200/60 p-2">
+                    <GrStrikeThrough className="invisible group-focus-within/note:visible hover:visible" />
+                  </button>
+                  <button className="invisible group-focus-within/note:visible hover:visible hover:bg-zinc-200/60 p-2">
+                    <GrUnorderedList className="invisible group-focus-within/note:visible hover:visible" />
+                  </button>
+                  <button className="invisible group-focus-within/note:visible hover:visible hover:bg-zinc-200/60 p-2">
+                    <GrImage className="invisible group-focus-within/note:visible hover:visible" />
+                  </button>
+                </div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        )}
+        <div
+          className={cn(
+            "flex flex-col bg-amber-100 border-amber-200 w-[220px] h-[200px] group-hover/note:bg-amber-950/10 group-hover/note:shadow-md",
+            isFolded === true ? "folded after:folded-amber-after" : ""
+          )}
+        >
           <div className="flex justify-between border-t-4 border-amber-200 group-hover/note:border-amber-950/5">
             <div></div>
             <div className="pr-2 pt-2 flex items-center">
-              <button>
+              {/* <button>
                 <PiDotsThree className="invisible group-hover/note:visible" />
-              </button>
+              </button> */}
+              <NoteMenu
+                open={openMenu}
+                key={count}
+                onOpen={() => {
+                  setIsFolded(() => true);
+                }}
+                onClose={() => {
+                  setIsFolded(() => false);
+                }}
+                hasOpened={isFolded}
+              />
               <span className="group-hover/note:hidden text-slate-600 text-xs">
                 25/05/65
               </span>
