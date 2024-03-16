b9146bc42bc7bb5a717955ffbaf677c001563e2a
luckkrit <luckkrit@hotmail.com>
2024-03-16 07:44:33 +0700

    add cancel button to image dialog

--- src/components/draft2.stories.tsx
+++ src/components/draft2.stories.tsx
@@ -464,6 +464,15 @@ const ResizableNote = forwardRef<HTMLDivElement, ResizableNoteProps>(
               >
                 Insert
               </button>
+
+              <button
+                onClick={() => {
+                  setIsOpen(() => false);
+                }}
+                className="float-right mr-4 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5"
+              >
+                Cancel
+              </button>
             </ImagePreviewDialog>
             <input
               type="file"
