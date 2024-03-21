66a28aee523631507785e2f39d7be71f95bdbfdb
luckkrit <luckkrit@hotmail.com>
2024-03-21 20:28:38 +0700

    update code and deploy

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,8 +1,8 @@
 import { NoteAction } from "@/hooks";
-import { getNotes, NoteModel } from "@/lib/db";
+import { NoteModel } from "@/lib/db";
 import React, { createContext, useContext } from "react";
 
-export const initialNotes: NoteModel[] = await getNotes();
+export const initialNotes: NoteModel[] = [];
 
 export const NotesContext = createContext<NoteModel[]>([]);
 export const NotesDispatchContext = createContext<React.Dispatch<NoteAction> | null>(
--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -1,9 +1,21 @@
 import { initialNotes, NotesContext, NotesDispatchContext } from "@/context";
 import { notesReducer } from "@/hooks";
-import { PropsWithChildren, useReducer } from "react";
+import { getNotes, NoteModel } from "@/lib/db";
+import { PropsWithChildren, useEffect, useReducer, useState } from "react";
 
 export const NotesProvider = ({ children }: PropsWithChildren) => {
-  const [notes, dispatch] = useReducer(notesReducer, initialNotes);
+  const [readNotes, setReadNotes] = useState<NoteModel[]>([]);
+  useEffect(() => {
+    const initNotes = async () => {
+      const n2 = await getNotes();
+      setReadNotes(() => n2);
+    };
+    initNotes();
+  }, []);
+  const [notes, dispatch] = useReducer(
+    notesReducer,
+    initialNotes.length === 0 ? readNotes : initialNotes
+  );
   return (
     <NotesContext.Provider value={notes}>
       <NotesDispatchContext.Provider value={dispatch}>
